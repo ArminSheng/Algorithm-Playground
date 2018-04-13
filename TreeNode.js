5f79b54bff70d84641d2e6b802d383d6fedb41cc
armin <arimin@qq.com>
2018-04-13 19:32:22 +0800

    depth first traverse of Binary Tree

--- TreeNode.js
+++ TreeNode.js
@@ -3,4 +3,14 @@ class TreeNode {
 		this.val = val;
 		this.left = this.right = null;
 	}
+
+  traverse (fn) {
+    const _traverse = (node, fn) => {
+      fn(node);
+      node.left && _traverse(node.left, fn);
+      node.right && _traverse(node.right, fn);
+    }
+
+    _traverse(this, fn);
+  }
 };
