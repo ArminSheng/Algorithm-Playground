a814f9240468130cd37460e0da80431ca14a60db
armin <arimin@qq.com>
2018-12-29 23:19:24 +0800

    add n

---
+++ js/problems:binary-tree-maximum-path-sum.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+    function dfs(root) {
+        if (!root) return null;
+
+        const val = root.val;
+        const left = dfs(root.left);
+        const right = dfs(root.right);
+        const sum = val + left + right;
+        const _max = Math.max(left + val, right + val, val);
+
+        max = Math.max.apply(null, [max, sum, _max]);
+        return _max;
+    }
+
+    let max = -Infinity;
+    dfs(root);
+    
+    return max;
+};
