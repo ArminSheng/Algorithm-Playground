faa95576438edb18637aa4290eaedc0d71fe8dc3
armin <arimin@qq.com>
2019-01-13 01:20:24 +0800

    add 207 course schedule

---
+++ js/course-schedule.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+    const ADJ = Array(numCourses).fill(0).map(_ => []);
+    const marked = [];
+    const onStack = [];
+    let hasCycle = false;
+
+    for (let e of prerequisites) {
+        ADJ[e[0]].push(e[1]);
+    }
+
+    function dfs(v) {
+        marked[v] = true;
+        onStack[v] = true;
+
+        for (let w of ADJ[v]) {
+            if (hasCycle) {
+                return;
+            } else if (!marked[w]) {
+                dfs(w);
+            } else if (onStack[w]) {
+                hasCycle = true;
+                return;
+            }
+        }
+
+        onStack[v] = false;
+    }
+
+    let i = -1;
+    while (++i < numCourses) {
+        if (hasCycle) return false;
+        if(!marked[i]) dfs(i);
+    }
+
+    return true; 
+};
