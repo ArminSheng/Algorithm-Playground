f8bf06ac0b33e9660bd5ac80c485ed9b010131ce
armin <arimin@qq.com>
2019-01-18 20:35:42 +0800

    update priority queue

--- js/PriorityQueue.js
+++ js/PriorityQueue.js
@@ -4,25 +4,30 @@
 class IndexPriorityQueue {
     constructor () {
         this.pq = [];
-        this.qp = {};
+        this.keys = {};
         this.N = 0;
     }
 
     insert (key, priority) {
-        const pq = this.pq;
-        const qp = this.qp;
+        const {pq, keys} = this;
+        const idx = this.includes(key);
+
+        if (idx) {
+            this.change(key, priority, idx);
+            return;
+        }
+
         pq[++this.N] = key;
-        qp[key] = priority;
+        keys[key] = priority;
 
         this.swim(this.N);
     }
 
     swim (k) {
-        const {pq, qp} = this;
+        const {pq, keys} = this;
         let p = k >> 1;
-        console.log(k,p,pq[k],pq[p], qp[pq[k]],qp[pq[p]]);
 
-        while (p > 0 && qp[pq[k]] < qp[pq[p]]) {
+        while (p > 0 && this.less(k, p)) {
             this.exch(k, p);
             k = p;
             p = p >> 1;
@@ -30,16 +35,20 @@ class IndexPriorityQueue {
     }
     
     sink (k) {
-        const {pq, qp, N} = this;
+        const {pq, keys, N} = this;
         let c = k * 2;
 
-        // if (c + 1 <= N && qp[pq[k]] > qp[pq[c + 1]]) c += 1;
-        while (c <= N && qp[pq[k]] > qp[pq[c]]) {
-            if (c + 1 <= N && qp[pq[c]] > qp[pq[c + 1]]) c += 1;
+        while (c <= N && this.less(c, k)) {  
+            if (c + 1 <= N && this.less(c + 1, c)) c++;
             this.exch(k, c);
             k = c;
             c = 2 * k;
         }
+    }
+
+    less (i, j) {
+        const {keys, pq} = this;
+        return keys[pq[i]] < keys[pq[j]];
     }
 
     exch (i, j) {
@@ -47,12 +56,25 @@ class IndexPriorityQueue {
         [pq[i], pq[j]] = [pq[j], pq[i]];
     }
 
+    includes (key) {
+        const idx = this.pq.indexOf(key);
+        return idx > -1 ? idx : 0;
+    }
+
+    change (key, priority, index) {
+        const [pq, keys] = this;
+
+        keys[key] = priority;
+        this.swim(index);
+        this.sink(index);
+    }
+
     delMin () {
-        if (!this.N < 1) return null;
+        if (this.N < 1) return null;
 
         const m = this.pq[1];
         delete this.pq[1];
-        delete this.qp[m];
+        delete this.keys[m];
 
         this.exch(this.N, 1)
         this.N--;
--- js/network-delay-time.js
+++ js/network-delay-time.js
@@ -29,18 +29,19 @@ var networkDelayTime = function(times, N, K) {
             if (distTo[to] > dis) {
                 distTo[to] = dis;
                 if (!queue.includes(to)) {
-                    queue.push(to);
+                    queue.insert(to, dis);
+                } else {
+                    queue.change(to, dis)
                 }
             }
         }
     }
 
     function dijkstra(v) {
-        const queue = [v];
-        
-        while (queue.length) {
-            v = queue.shift();
+        const queue = new IndexPriorityQueue();
+        while (v) {
             relax(v, queue);
+            v = queue.delMin();
         }
     }
 
@@ -50,27 +51,36 @@ var networkDelayTime = function(times, N, K) {
     return res === Infinity ? -1 : res;
 };
 
+/**
+ * 索引优先队列
+ */
 class IndexPriorityQueue {
     constructor () {
         this.pq = [];
-        this.qp = {};
+        this.keys = {};
         this.N = 0;
     }
 
     insert (key, priority) {
-        const pq = this.pq;
-        const qp = this.qp;
+        const {pq, keys} = this;
+        const idx = this.includes(key);
+
+        if (idx) {
+            this.change(key, priority, idx);
+            return;
+        }
+
         pq[++this.N] = key;
-        qp[key] = priority;
+        keys[key] = priority;
 
         this.swim(this.N);
     }
 
     swim (k) {
-        const {pq, qp} = this;
+        const {pq, keys} = this;
         let p = k >> 1;
 
-        while (p > 0 && qp[pq[k]] < qp[pq[p]]) {
+        while (p > 0 && this.less(k, p)) {
             this.exch(k, p);
             k = p;
             p = p >> 1;
@@ -78,15 +88,20 @@ class IndexPriorityQueue {
     }
     
     sink (k) {
-        const {pq, qp, N} = this;
+        const {pq, keys, N} = this;
         let c = k * 2;
-        console.log(k,c,pq[k],pq[c], qp[pq[k]],qp[pq[c]]);
-        while (c <= N && qp[pq[k]] > qp[pq[c]]) {
-            if (k + 1 <= N && qp[pq[k]] > qp[pq[c + 1]]) c += 1;
+
+        while (c <= N && this.less(c, k)) {  
+            if (c + 1 <= N && this.less(c + 1, c)) c++;
             this.exch(k, c);
             k = c;
             c = 2 * k;
         }
+    }
+
+    less (i, j) {
+        const {keys, pq} = this;
+        return keys[pq[i]] < keys[pq[j]];
     }
 
     exch (i, j) {
@@ -94,10 +109,29 @@ class IndexPriorityQueue {
         [pq[i], pq[j]] = [pq[j], pq[i]];
     }
 
+    includes (key) {
+        const idx = this.pq.indexOf(key);
+        return idx > -1 ? idx : 0;
+    }
+
+    change (key, priority, index) {
+        const {pq, keys} = this;
+
+        keys[key] = priority;
+        this.swim(index);
+        this.sink(index);
+    }
+
+    isEmpty () {
+        return this.N > 0;
+    }
+
     delMin () {
+        if (this.N < 1) return null;
+
         const m = this.pq[1];
         delete this.pq[1];
-        delete this.qp[m];
+        delete this.keys[m];
 
         this.exch(this.N, 1)
         this.N--;
@@ -106,10 +140,3 @@ class IndexPriorityQueue {
         return m;
     }
 }
-
-pq = new IndexPriorityQueue();
-pq.insert('a', 1);
-pq.insert('d', 4);
-pq.insert('b', 2);
-pq.insert('c', 3);
-pq.insert('e', 5);
