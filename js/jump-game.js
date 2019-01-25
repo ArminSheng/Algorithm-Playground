39688270b8be91666a6c775aae97a8e68042b3ab
Armin Sheng <15288119054@163.com>
2019-01-25 11:58:31 +0800

    update jump game

--- js/jump-game.js
+++ js/jump-game.js
@@ -3,28 +3,42 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    let pos, dist, idx, max = 0, cur = 0;
+    let end = 0;
+    const len = nums.length;
+
+    for (let i = 0; i < len && i <= end; i++) {
+        end = Math.max(end, i + nums[i]);
+    }
+
+    return end >= len - 1;
+};
+
+var canJump2 = function(nums) {
     const L = nums.length;
+    if (L < 2) return true;
 
-    while (cur < L) {
-        pos = cur + nums[cur];
+    let pos, end, idx, max = 0, start = 0, steps = nums[0];
+
+    while (steps > 0) {
+        max = pos = start + nums[start];
+        idx = start;
         if (pos >= L - 1) return true;
-        dist = cur + nums[cur];
+        end = start + steps;
 
-        while (cur < dist) {
-            // console.log(pos, nums[i + k + 1], i + k + 1);
-            cur++;
-            pos = cur + nums[cur];
-            // if (pos >= L - 1) return true;
+        while (start < end) {
+            start++;
+            pos = start + nums[start];
+
             if (isNaN(pos)) return true;
-            if (pos > max) {
+            if (pos >= max) {
                 max = pos;
-                idx = cur;
+                idx = start;
             }
         }
 
-        // console.log(pos)
-        cur = idx;
-        if (nums[cur] === 0) return false;
+        start = idx;
+        steps = nums[start];
     }
+
+    return false;
 };
