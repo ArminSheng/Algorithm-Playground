07500641ae0d32ac63e419a64151c091ddb567a0
armin <arimin@qq.com>
2019-02-02 22:49:54 +0800

    merge

--- js/candy.js
+++ js/candy.js
@@ -3,30 +3,55 @@
  * @return {number}
  */
 var candy = function(ratings) {
-    const L = ratings.length;
-    let min = ratings[0], minI = 0;
     const candy = Array(ratings.length).fill(1);
 
-    for (let i = 0; i < L; i++) {
-        if (ratings[i] < min) {
-            min = ratings[i];
-            minI = i;
-        }
-    }
-
-    for (let i = minI + 1; i < L; i++) {
+    for (let i = 1; i < ratings.length; i++) {
         if (ratings[i] > ratings[i - 1]) {
-            candy[i] = candy[i - 1] + 1;
-        }
+            candy[i] = Math.max(candy[i - 1] + 1, candy[i]);
+        } 
     }
 
-    for (let i = minI - 1; i > 0; i--) {
+    for (let i = ratings.length - 2; i >= 0; i--) {
         if (ratings[i] > ratings[i + 1]) {
-            candy[i] = candy[i + 1] + 1;
+            candy[i] = Math.max(candy[i + 1] + 1, candy[i]);
         }
     }
 
-    let sum = candy.reduce((cur, acc) => acc + cur, 0);
+    return candy.reduce((cur, acc) => cur + acc, 0);
+}
 
-    return sum;
+var candy2 = function(ratings) {
+    function findMin () {
+        let min;
+        let minVal;
+        ratings.forEach((r, i) => {
+            if (!candy[i] && (min === undefined || r < minVal)) {
+                minVal = r;
+                min = i;
+            }
+        });
+
+        return min;
+    }
+
+    const candy = Array(ratings.length).fill(0);
+    let start = findMin();
+    
+    while(start !== undefined) {
+        candy[start] = 1;
+        for (let i = start + 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candy[i] = Math.max(candy[i - 1] + 1, candy[i]);
+            } else break;
+        }
+
+        for (let i = start - 1; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candy[i] = Math.max(candy[i + 1] + 1, candy[i]);
+            } else break;
+        }
+
+        start = findMin();
+    }
+    return candy.reduce((cur, acc) => cur + acc, 0);
 };
