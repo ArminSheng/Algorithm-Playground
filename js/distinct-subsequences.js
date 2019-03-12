c33461c57567a9989cb5e6672addaeb7364b6f77
armin <arimin@qq.com>
2019-03-12 23:25:08 +0800

    update 115

--- js/distinct-subsequences.js
+++ js/distinct-subsequences.js
@@ -7,33 +7,43 @@ var numDistinct = function(s, t) {
     const m = s.length;
     const n = t.length;
 
-    const dp = Array(m).fill(0).map(_ => Array(n).fill(0));
+    const dp = Array(m + 1).fill(0).map(_ => []);
 
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (s[i] === t[j]) {
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (j === 0) dp[i][j] = 1;
 
+            else if (i < j) dp[i][j] = 0;
+
+            else if (s[i - 1] === t[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            } 
+            
+            else {
+                dp[i][j] = dp[i - 1][j];
             }
         }
     }
+
+    return dp[m][n];
 }
 
 
 
 // @Deprecated
-var numDistinct1 = function(s, t) {
-    let res = 0;
+// var numDistinct1 = function(s, t) {
+//     let res = 0;
 
-    function backtrack(i, start) {
-        if (i === t.length) res++;
-        else {
-            let c = t[i];
-            for (let j = start + 1; j < s.length; j++) {
-                if (s[j] === c) backtrack(i + 1, j);
-            }
-        }
-    }
+//     function backtrack(i, start) {
+//         if (i === t.length) res++;
+//         else {
+//             let c = t[i];
+//             for (let j = start + 1; j < s.length; j++) {
+//                 if (s[j] === c) backtrack(i + 1, j);
+//             }
+//         }
+//     }
 
-    backtrack(0, -1);
-    return res;
-};
+//     backtrack(0, -1);
+//     return res;
+// };
