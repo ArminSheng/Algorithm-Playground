dd112bf6289bbbdb6d76fb419e81564f00ab0ef4
armin <arimin@qq.com>
2019-03-13 19:38:27 +0800

    update 152

--- js/maximum-product-subarray.js
+++ js/maximum-product-subarray.js
@@ -4,14 +4,41 @@
  */
 var maxProduct = function(nums) {
     if (!nums.length) return 0;
-    const dp = [nums[0]];
-    let acc = nums[0];
-    let res = -Infinity;
 
-    for (let i = 1; i < nums.length; i++) {
+    const max = function (nums) {
+        const dp = nums.slice();
+
+        for (let i = 1; i < nums.length; i++) {
+            dp[i] *= dp[i - 1] || 1;
+        }
+
+        return Math.max(...dp);
+    }
+
+    return Math.max(max(nums), max(nums.reverse()));
+};
+
+
+var maxProduct1 = function(nums) {
+    const len = nums.length;
+    if (!len) return 0;
+
+    let acc = nums[0];
+    let res = acc;
+    let max = acc;
+
+    for (let i = 1; i < len; i++) {
         acc = nums[i - 1] === 0 ? nums[i] : acc * nums[i];
-        dp[i] = dp[i - 1] > 0 ? dp[i - 1] * nums[i] : nums[i];
-        res = Math.max(res, dp[i], acc);
+        max = max > 0 ? max * nums[i] : nums[i];
+        res = Math.max(res, max, acc);
+    }
+
+    acc = nums[len - 1];
+    max = acc;
+    for (let i = len - 2; i > 0; i--) {
+        acc = nums[i + 1] === 0 ? nums[i] : acc * nums[i];
+        max = max > 0 ? max * nums[i] : nums[i];
+        res = Math.max(res, max, acc);
     }
 
     return res;
