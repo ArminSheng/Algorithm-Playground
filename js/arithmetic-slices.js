4c11836d4eb1675609e2b7d77b615a193f70764d
Armin Sheng <15288119054@163.com>
2019-03-22 15:26:29 +0800

    update 413

--- js/arithmetic-slices.js
+++ js/arithmetic-slices.js
@@ -3,6 +3,26 @@
  * @return {number}
  */
 var numberOfArithmeticSlices = function(A) {
+    const dp = [0, 0];
+
+    let gap1 = A[1] - A[0];
+    let gap2;
+
+    for (let i = 2; i < A.length; i++) {
+        gap2 = A[i] - A[i - 1];
+
+        if (gap1 === gap2) {
+            dp[i] = dp[i - 1] + 1;
+        } else {
+            dp[i] = 0;
+            gap1 = gap2;
+        }
+    }
+
+    return dp.reduce((cur, acc) => acc + cur, 0);
+}
+
+var numberOfArithmeticSlices = function(A) {
     const l = A.length;
     if (l < 3) return 0;
 
