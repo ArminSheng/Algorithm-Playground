d01caa2d519fb8226e1b3ea99c8515cc0aaefbfc
Armin Sheng <15288119054@163.com>
2019-03-27 16:34:31 +0800

    udate 61

--- js/rotate-list.js
+++ js/rotate-list.js
@@ -11,6 +11,34 @@
  * @return {ListNode}
  */
 var rotateRight = function(head, k) {
+    if (!head || !head.next || !k) return head;
+    let h = head;
+    let tail, prev;
+    let len = 0;
+
+    while (head) {
+        len++;
+        tail = head;
+        head = head.next;
+    }
+
+    let n = len - (k % len);
+
+    if (n === len) return h;
+
+    head = h;
+    while (n > 0) {
+        n--;
+        prev = head;
+        head = head.next;
+    }
+
+    prev.next = null;
+    tail.next = h;
+    return head;
+}
+
+var rotateRight1 = function(head, k) {
     if (!head) return head;
     const nodes = [];
     const vals = [];
