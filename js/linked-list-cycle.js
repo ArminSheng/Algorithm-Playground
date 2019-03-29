0847dbead3d59e583c81158f3e8e435e3b35e439
armin <arimin@qq.com>
2019-03-29 14:09:49 +0800

    add: 141 linked-list-cycle

---
+++ js/linked-list-cycle.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+//  快慢指针
+var hasCycle = function(head) {
+    const set = new Set;
+
+    while (head) {
+        if (set.has(head)) return true;
+
+        set.add(head);
+        head = head.next;
+    }
+
+    return false;
+}
+
+var hasCycle1 = function(head) {
+    let fast = head;
+    let slow = head;
+    
+    while (fast) {
+        fast = fast.next;
+        slow = slow.next;
+        
+        if (!fast) return false;
+        
+        fast = fast.next;
+        
+        if (fast === slow) return true;
+    }
+    
+    return false;
+};
