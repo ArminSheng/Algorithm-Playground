82a12ff06ee2a7a527f6ce77f4564bee892c7fe5
Armin Sheng <15288119054@163.com>
2019-04-08 16:11:18 +0800

    update 430

--- js/flatten-a-multilevel-doubly-linked-list.js
+++ js/flatten-a-multilevel-doubly-linked-list.js
@@ -12,7 +12,42 @@
  * @return {Node}
  */
 
-var flatten1 = function(head) {
+var flatten = function(head) {
+
+    function dfs (head) {
+        if (!head) return head;
+        let h = head;
+
+        while (h) {
+            if (h.child) {
+                const next = h.next;
+                const child = h.child;
+                const tail = dfs(child);
+
+                if (tail) {
+                    tail.next = next;
+                    if (next) {
+                        next.prev = tail;
+                    }
+                    child.prev = h;
+                }
+
+                h.next = child;
+                h.child = null;
+            }
+
+            if (h.next) h = h.next;
+            else break;
+        }
+
+        return h;
+    }
+    dfs (head);
+
+    return head;
+}
+
+var flatten2 = function(head) {
     const arr = [];
     function dfs (head) {
         if (!head) return head;
@@ -34,7 +69,7 @@ var flatten1 = function(head) {
     return arr[0] || null;
 }
 
-var flatten = function(head) {
+var flatten1 = function(head) {
     if (!head) return null;
     const next = flatten(head.next);
     const child = flatten(head.child);
