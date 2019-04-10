d472cb7346d15afcd41fbdff5c0ba8d69c0ad58b
Armin Sheng <15288119054@163.com>
2019-04-10 16:51:19 +0800

    update: 160

--- js/intersection-of-two-linked-lists.js
+++ js/intersection-of-two-linked-lists.js
@@ -11,7 +11,56 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
+
+//  Best solution
 var getIntersectionNode = function(headA, headB) {
+    let a = headA;
+    let b = headB;
+
+    while (a !== b) {
+        a = a ? a.next : headB;
+        b = b ? b.next : headA;
+    }
+
+    return a;
+}
+
+var getIntersectionNode2 = function(headA, headB) {
+    let a = headA;
+    let b = headB;
+    let c;
+    let d;
+    let count = 0;
+
+    while (a || b) {
+        if (a === b) return a;
+        if (!a || !b) {
+            count++;
+            c = a ? headA : headB;
+            d = a ? headB : headA;
+        }
+
+        a = a && a.next;
+        b = b && b.next;
+    }
+
+    while (count) {
+        c = c.next;
+        count--;
+    }
+
+    while (c) {
+        if (c === d) return c;
+
+        c = c.next;
+        d = d.next;
+    }
+
+    return null;
+}
+
+// Stupid solution
+var getIntersectionNode1 = function(headA, headB) {
     let a = headA;
     let b = headB;
 
