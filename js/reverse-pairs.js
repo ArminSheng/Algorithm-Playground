c9bbd6876f1c8dbd62278ca73feca7e0fd88e494
Armin Sheng <15288119054@163.com>
2019-05-27 11:20:54 +0800

    update 493

--- js/reverse-pairs.js
+++ js/reverse-pairs.js
@@ -4,39 +4,56 @@
  */
 
 var reversePairs = function(nums) {
+    let count = 0;
 
-}
-const mergeSort = function (arr) {
-    const len = arr.length;
-    if (len < 2) return arr;
+    const mergeSort = function (arr) {
+        const len = arr.length;
+        if (len < 2) return arr;
 
-    const mid = len >> 1;
-    const left = mergeSort(arr.slice(0, mid));
-    const right = mergeSort(arr.slice(mid));
+        const mid = len >> 1;
+        const left = mergeSort(arr.slice(0, mid));
+        const right = mergeSort(arr.slice(mid));
 
-    return merge(left, right);
-}
+        let i = 0;
+        let j = mid;
 
-const merge = function (lt, ri) {
-    const tmp = [];
-    let i = 0;
-    let j = 0;
+        while (i < mid && j < len) {
+            if (left[i] > 2 * right[j]) {
+                count += mid - i;
+                j++;
+            } else {
+                i++;
+            }
+        }
 
-    while (i < lt.length && j < ri.length) {
-        if (lt[i] < ri[j]) {
-            tmp.push(lt[i]);
-            i++;
-        } else {
-            tmp.push(ri[j]);
-            j++;
+        return merge(left, right);
+    }
+
+    const merge = function (lt, ri) {
+        const tmp = [];
+        let i = 0;
+        let j = 0;
+
+        while (i < lt.length && j < ri.length) {
+            if (lt[i] < ri[j]) {
+                tmp.push(lt[i]);
+                i++;
+            } else {
+                tmp.push(ri[j]);
+                j++;
+            }
+        }
+
+        if (i < lt.length) {
+            return tmp.concat(lt.slice(i));
+        } else if (j < ri.length) {
+            return tmp.concat(ri.slice(j));
         }
     }
 
-    if (i < lt.length) {
-        return tmp.concat(lt.slice(i));
-    } else if (j < ri.length) {
-        return tmp.concat(ri.slice(j));
-    }
+    mergeSort(nums);
+
+    return count;
 }
 
 //  Error
