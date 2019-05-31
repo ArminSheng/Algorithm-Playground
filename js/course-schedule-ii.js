90651056e96ff541900ee4a548351f23bde9bdc0
Armin Sheng <15288119054@163.com>
2019-05-31 14:56:59 +0800

    update 210

--- js/course-schedule-ii.js
+++ js/course-schedule-ii.js
@@ -3,7 +3,42 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
+
+//  Greedy
 var findOrder = function(numCourses, prerequisites) {
+    const inDegrees = Array(numCourses).fill(0);
+    const res = [];
+    const G = Array(numCourses).fill(0).map(_ => []);
+
+    for (let e of prerequisites) {
+        G[e[1]].push(e[0]);
+        inDegrees[e[0]]++;
+    }
+
+    while (res.length < numCourses) {
+        let hasCycle = true;
+
+        for (let i = 0; i < numCourses; i++) {
+            if (inDegrees[i] === 0) {
+                hasCycle = false;
+
+                // 入度设为-1代表该节点已输出至result
+                inDegrees[i] = -1;
+                res.push(i);
+
+                for (let w of G[i]) {
+                    inDegrees[w]--;
+                }
+            }
+        }
+
+        if (hasCycle) return [];
+    }
+
+    return res;
+}
+
+var findOrder1 = function(numCourses, prerequisites) {
     const G = Array(numCourses).fill(0).map(_ => []);
     const marked = [];
     let hasCycle = false;
