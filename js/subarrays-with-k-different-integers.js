5131444612c37699a13636c6d639def41683a9c1
Armin Sheng <15288119054@163.com>
2019-07-03 11:30:22 +0800

    update 992: add explain

--- js/subarrays-with-k-different-integers.js
+++ js/subarrays-with-k-different-integers.js
@@ -3,6 +3,13 @@
  * @param {number} K
  * @return {number}
  */
+
+//  Assume 2 windows, one is larger than the other, they ends with rt,
+//  and starts with l1, l2 respectively.
+//  the larger one should contain maximum amounts of number which make the window
+//  as a 'good' array, and the smaller one should contain minimum amount.
+//  The amount of result in the current window equals the quantity difference between
+//  the 2 windows which consist of the last k - 1 elements. which is l2 - l1.
 var subarraysWithKDistinct = function(A, K) {
     const len = A.length;
     const map1 = Array(len + 1).fill(0);
