5e035ba72d8c47f09c2dc48dcbfcd51cedf01cbe
Armin Sheng <15288119054@163.com>
2019-07-15 10:43:03 +0800

    add 20

--- js/valid-parentheses.js
+++ js/valid-parentheses.js
@@ -3,5 +3,22 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    if (!s) return true;
 
+    const stack = [s[0]];
+    const map = {
+        ']': '[',
+        ')': '(',
+        '}': '{',
+    };
+
+    for (let i = 1; i < s.length; i++) {
+        if (map[s[i]] && map[s[i]] === stack[stack.length - 1]) {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
+
+    return !stack.length;
 };
