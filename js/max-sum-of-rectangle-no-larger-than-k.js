fdf18e4226f95022d01b2c62d4585dd94aaaf730
Armin Sheng <15288119054@163.com>
2019-08-14 12:04:21 +0800

    add 363

--- js/max-sum-of-rectangle-no-larger-than-k.js
+++ js/max-sum-of-rectangle-no-larger-than-k.js
@@ -10,65 +10,54 @@ function maxSumSubmatrix (matrix, K) {
 
     for (let i = 0; i < n; i++) {
         const rowSum = Array(m).fill(0);
-        for (let j = i; j < n; j++) {
 
+        for (let j = i; j < n; j++) {
             for (let k = 0; k < m; k++) {
                 rowSum[k] += matrix[k][j];
-                let sum = 0;
-                const arr = [0];
+            }
 
-                for (let r = 0; r < m; r++) {
-                    sum += rowSum[r];
-                    const idx = upperBound(arr, sum - K);
+            let sum = 0;
+            const arr = [0];
 
-                    if (idx > -1) {
-                        if (sum - arr[idx] === K) return K;
-                        else max = Math.max(max, sum - arr[idx]);
-                    }
+            for (let r = 0; r < m; r++) {
+                sum += rowSum[r];
+                let idx = insertIndex(arr, sum - K);
+                idx = idx >= arr.length ? arr.length - 1 : idx;
+                const val = sum - arr[idx];
 
-                    arr.push(sum);
-                    arr.sort((a, b) => a - b);
+                if (idx > -1 && val <= K) {
+                    if (val === K) return K;
+                    else max = Math.max(max, val);
                 }
 
+                const insertIdx = insertIndex(arr, sum);
+                if (arr[insertIdx] !== sum) {
+                    arr.splice(insertIdx, 0, sum);
+                }
             }
+
         }
     }
 
     return max;
 }
 
-// const binarySearchLowerBound = function (arr, target) {
-//     let low = 0;
-//     let high = arr.length;
-//     let mid = 0;
+function insertIndex(nums, target){
+    let low = 0;
+    let high = nums.length - 1;
+    let mid;
 
-//     while (low < high) {
-//         mid = (low + high) >> 1;
-//         if (arr[mid] > target) low = mid + 1;
-//         else high = mid;
-//     }
+    while (low <= high) {
+        mid = (low + high) >> 1;
 
-//     return low;
-// }
-
-function upperBound(a, key){
-    let s = 0, e = a.length - 1;
-    let ans = -1;
-
-    while(s <= e) {
-        let mid = (s + e) >> 1;
-
-        if(a[mid] == key){
-            ans = mid;
-            s = mid+1;
-        }
-        else if(a[mid] > key){
-            e = mid - 1;
-        }
-        else{
-            s = mid + 1;
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
     }
 
-    return ans;
+    return low;
 }
