4f2c65e0230069515c50fc512c8bdc1537a99982
Armin Sheng <15288119054@163.com>
2019-09-03 12:30:58 +0800

    update 827

--- js/making-a-large-island.js
+++ js/making-a-large-island.js
@@ -1,43 +1,71 @@
 /**
  * @param {number[][]} grid
  * @return {number}
+ * 通过并查集(union find)这种数据结构找出所有的岛屿及其面积
+ * grid[i][j] === 0时，找出与该点相连的union面积之和，与最大面积进行比较
  */
 var largestIsland = function(grid) {
     const m = grid.length;
     if (!m) return 0;
 
     let res = 0;
-    let count;
+    const areas = [0, 0];
 
+    // 寻找所有的岛屿并记录面积
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < m; j++) {
-            if (grid[i][j] === 0) {
-                grid[i][j] = 1;
-
-                const map = Array(m).fill(0).map(_ => []);
-                count = 0;
-
-                dfs(grid, i, j, map, m)
-                res = Math.max(res, count);
-
-                grid[i][j] = 0;
+            if (grid[i][j] === 1) {
+                const index = areas.length;
+                areas[index] = dfs_union(grid, i, j, index, m);
             }
         }
     }
 
-    function dfs (grid, i, j, map, m) {
-        if (i >= m || i < 0 || j >= m || j < 0 || map[i][j]) return count;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < m; j++) {
+            if (grid[i][j] === 0) {
+                let ans = 1;
 
-        map[i][j] = true;
+                for (let w of neighbors(i, j, grid, m)) {
+                    ans += areas[w];
+                }
 
-        if (grid[i][j] === 1) {
-            count++;
-            dfs(grid, i + 1, j, map, m);
-            dfs(grid, i - 1, j, map, m);
-            dfs(grid, i, j + 1, map, m);
-            dfs(grid, i, j - 1, map, m);
+                res = Math.max(res, ans);
+            }
         }
     }
 
     return res === 0 ? m * m : res;
 };
+
+// 找出与grid[i][j]相邻的union
+// @return {Set}
+function neighbors (i, j, grid, m) {
+    const set = new Set();
+
+    if (i < m - 1 && grid[i + 1][j]) set.add(grid[i + 1][j]);
+    if (i > 0 && grid[i - 1][j]) set.add(grid[i - 1][j]);
+    if (grid[i][j + 1]) set.add(grid[i][j + 1]);
+    if (grid[i][j - 1]) set.add(grid[i][j - 1]);
+
+    return set;
+}
+
+// @return {number}
+function dfs_union (grid, i, j, n, size) {
+    if (i >= size || i < 0 || j >= size || j < 0 || grid[i][j] === 0) return 0;
+
+    let count = 0;
+
+    if (grid[i][j] !== n) {
+        grid[i][j] = n;
+        count++;
+
+        count += dfs_union(grid, i + 1, j, n, size);
+        count += dfs_union(grid, i - 1, j, n, size);
+        count += dfs_union(grid, i, j + 1, n, size);
+        count += dfs_union(grid, i, j - 1, n, size);
+    }
+
+    return count;
+}
