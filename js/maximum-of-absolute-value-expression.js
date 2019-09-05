aff6a3ca309fc43b657ba4a09d719f0ab87a69ee
Armin Sheng <15288119054@163.com>
2019-09-05 15:43:32 +0800

    update 1131

--- js/maximum-of-absolute-value-expression.js
+++ js/maximum-of-absolute-value-expression.js
@@ -5,6 +5,27 @@
  */
 var maxAbsValExpr = function(arr1, arr2) {
     const len = arr1.length;
+    const a = [], b = [], c = [], d = [];
+    const max = Math.max;
+    const min = Math.min;
+
+    for (let i = 0; i < len; i++) {
+        a.push(arr1[i] + arr2[i] + i);
+        b.push(arr1[i] - arr2[i] + i);
+        c.push(arr1[i] - arr2[i] - i);
+        d.push(arr1[i] + arr2[i] - i);
+    }
+
+    const maxA = max(...a) - min(...a);
+    const maxB = max(...b) - min(...b);
+    const maxC = max(...c) - min(...c);
+    const maxD = max(...d) - min(...d);
+
+    return max(maxA, maxB, maxC, maxD);
+}
+
+var maxAbsValExpr = function(arr1, arr2) {
+    const len = arr1.length;
     let ans = 0;
     let sum;
 
