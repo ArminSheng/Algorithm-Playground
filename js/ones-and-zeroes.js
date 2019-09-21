d3de8a5b8c0904d1743c86d276d0de3583703618
armin <arimin@qq.com>
2019-09-21 10:28:49 +0800

    add 474

---
+++ js/ones-and-zeroes.js
@@ -0,0 +1,84 @@
+/**
+ * @param {string[]} strs
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var findMaxForm = function(strs, m, n) {
+    const map = {};
+    for (let s of strs) {
+        if (map[s]) continue;
+        
+        map[s] = [0, 0];
+        for (let c of s) {
+            map[s][c]++;
+        }
+    }
+
+    const s1 = strs.sort((a, b) => {
+        if (a.length === b.length) return map[a][0] - map[b][0];
+        
+        return a.length - b.length;
+    });
+
+    const v1 = h(s1, m, n);
+    const v2 = h(s1.sort((a, b) => {
+            if (a.length === b.length) return map[a][1] - map[b][1];
+        
+            return a.length - b.length;
+        }), m, n);
+
+    const v3 = h(s1.sort((a, b) => map[a][0] - map[b][0]), m, n);
+    const v4 = h(s1.sort((a, b) => map[a][1] - map[b][1]), m, n);
+
+    return Math.max(v1, v2, v3, v4);
+};
+
+function h(strs, m, n) {
+    const len = strs.length;
+    let res = 0;
+    let s;
+    const remains = [m, n];
+
+    for (let i = 0; i < len; i++) {
+        s = strs[i];
+        const amount = [0, 0];
+        let shouldContinue = false;
+
+        for (let j of s) {
+            amount[j]++;
+            if (amount[j] > remains[j]) {
+                shouldContinue = true;
+                break;
+            }
+        }
+
+        if (shouldContinue) continue;
+
+        remains[0] -= amount[0];
+        remains[1] -= amount[1];
+
+        res++;
+    }
+
+    return res;
+}
+
+function bagProblem (arr, c) {
+    const k = arr.length;
+    const dp = Array(k + 1).fill(0).map(_ => Array(c + 1).fill(0));
+    let res = 0;
+    for (let i = 1; i <= k; i++) {
+        for (let j = 1; j <= c; j++) {
+            if (arr[i - 1][0] > j) dp[i][j] = dp[i - 1][j];
+            else {
+                const v1 = dp[i - 1][j];
+                const v2 = dp[i - 1][j - arr[i - 1][0]] + arr[i - 1][1];
+                dp[i][j] = Math.max(v1, v2);
+            }
+            
+            res = Math.max(res, dp[i][j]);
+        }
+    }
+    return res;
+}
