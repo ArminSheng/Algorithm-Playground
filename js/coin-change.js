f6594cb800f8e81d7a9bcbd9421402574069181e
Armin Sheng <15288119054@163.com>
2019-10-14 14:50:05 +0800

    update 322

--- js/coin-change.js
+++ js/coin-change.js
@@ -3,29 +3,28 @@
  * @param {number} amount
  * @return {number}
  */
+
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+
 var coinChange = function(coins, amount) {
     if (amount <= 0) return 0;
 
     const n = coins.length;
     const m = amount + 1;
-    const dp = Array(m).fill(0).map(_ => Array(n).fill(Infinity));
-    let res = Infinity;
+    const dp = Array(m).fill(Infinity);
+    dp[0] = 0;
 
     for (let i = 1; i < m; i++) {
         for (let j = 0; j < n; j++) {
             let c = coins[j];
-            if (i % c === 0) {
-                dp[i][j] = i / c;
 
-            }
-
-            dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] || Infinity, (i >= c ? dp[i - c][n - 1] : Infinity) + 1);
-
-            if (i === amount) {
-                res = Math.min(res, dp[i][j]);
-            }
+            dp[i] = Math.min(dp[i], (i >= c ? dp[i - c] : Infinity) + 1);
         }
     }
 
-    return res === Infinity ? -1 : res;
+    return dp[amount] === Infinity ? -1 : dp[amount];
 };
